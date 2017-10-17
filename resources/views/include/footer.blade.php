9c904c3d32fa14e332011e0f9a23bbcf4e05b17e
scarulli <scarulli.sara@gmail.com>
2017-10-17 19:03:05 +0200

    Border footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,5 +1,5 @@
 @section("footer")
-<footer class="w-100 p-2 container-fluid bg-leonardo">
+<footer class="w-100 p-2 container-fluid bg-leonardo border border-bottom-0 border-left-0 border-right-0 border-dark">
 	<div class="container-fluid">
     <div class="row">
     	<div class="col mt-1 mr-1">
